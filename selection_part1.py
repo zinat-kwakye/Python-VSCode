222c9b3d9096744f2ce817c19ca5dce7a0ee926a
zinat-kwakye <157800959+zinat-kwakye@users.noreply.github.com>
2024-03-12 16:45:59 +0000

    Add files via upload

---
+++ selection_part1.py
@@ -0,0 +1,27 @@
+#Part1
+
+#if statements - age verification - lines 5 to 14
+
+#age = int(input("Please enter your age: "))
+
+#if age >= 18:
+   # print("You are in category A.")
+
+#if age >= 16:
+    #print("You are in category B.")
+
+#if age < 16:
+   # print("You are in category C.")
+
+#elif statements - age verification - lines 18 to 27
+
+#age = int(input("Please enter your age: "))
+
+#if age >= 18:
+    #print("You are in category A.")
+
+#elif age >= 16:
+   # print("You are in category B.")
+
+#else:
+   # print("You are in category C.")
