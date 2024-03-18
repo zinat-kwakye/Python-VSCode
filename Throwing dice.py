9bc90dc3be60ee04712cb46708a43eb6fca442fa
zinat-kwakye <157800959+zinat-kwakye@users.noreply.github.com>
2024-03-18 10:14:26 +0000

    Add files via upload
    Morning exercise - modules

---
+++ Throwing dice.py
@@ -0,0 +1,8 @@
+#Throwing dice
+
+import dice
+
+print("Output 1 is:", dice.dice())
+
+print("Output 2 is:", dice.dice())
+
---
+++ dice.py
@@ -0,0 +1,5 @@
+import math
+import random
+
+def dice():
+    return random.randint(1,6)
